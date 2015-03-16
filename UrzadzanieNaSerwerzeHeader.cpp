c491641afaddf4d7cd952ee8a38b104cafecbc1b
macklo <maciek2608@gmail.com>
2015-03-16 23:11:07 +0100

    Dodano urządzenie na serwerze
    Przeznaczone do dalszego rozwoju.

--- UrzadzanieNaSerwerzeHeader.cpp
+++ UrzadzanieNaSerwerzeHeader.cpp
@@ -1,9 +1,61 @@
-//
-//  UrzadzanieNaSerwerzeHeader.cpp
-//  Komunikator_PROI_B
-//
-//  Created by Konto Mateusza on 11.03.2015.
-//  Copyright (c) 2015 Mateusz. All rights reserved.
-//
-
 #include "UrzadzanieNaSerwerzeHeader.h"
+
+ServerDevice::ServerDevice(IPID* a){
+	ID = *a;
+}
+
+ServerDevice::ServerDevice(Obiekt* obiekt){
+	ID = *(obiekt->getID());
+}
+
+
+const IPID* ServerDevice::getID(){
+	return &ID;
+}
+
+bool ServerDevice::checkAccess(Obiekt* toCheck){
+	for(std::vector<Obiekt*>::iterator it = AccessList.begin(); it != AccessList.end(); it++){
+		if(*it == toCheck) return true;
+	}
+	return false;
+}
+
+bool ServerDevice::checkAccess(IPID* toCheck){
+	for(std::vector<Obiekt*>::iterator it = AccessList.begin(); it != AccessList.end(); it++){
+		if((*it)->getID() == toCheck) return true;
+	}
+	return false;
+}
+
+void ServerDevice::addAccess(Obiekt* toAdd){
+	AccessList.push_back(toAdd);
+}
+
+void ServerDevice::addFuncionality(int toAdd){
+	FuctionalityList.push_back(toAdd);
+}
+
+bool ServerDevice::checkFunctionality(int toCheck){
+	for(std::vector<int>::iterator it = FuctionalityList.begin(); it != FuctionalityList.end(); it++){
+		if(*it == toCheck) return true;
+	}
+	return false;
+}
+
+
+bool ServerDevice::operator==(ServerDevice* SD){
+	return this->getID() == SD->getID();
+}
+
+void ServerDevice::operator<<(std::ostream &stream){
+	stream<<"Urzadzenie na serwerze: "<<std::endl<<&ID<<"Funkcjonalnosc:"<<std::endl;
+	for(std::vector<int>::iterator it = FuctionalityList.begin(); it != FuctionalityList.end(); it++){
+		stream<<"\t"<<*it<<std::endl;
+	}
+	stream<<"Dostepnosc:"<<std::endl;
+	for(std::vector<Obiekt*>::iterator it = AccessList.begin(); it != AccessList.end(); it++){
+		stream<<"\t";
+		**it<<(stream);
+		stream<<std::endl;
+	}
+}
